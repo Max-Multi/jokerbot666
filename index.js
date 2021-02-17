ffbb815c5e6ef842c0fed6b797edaecae0b578a1
StunxFS <advancestunx@hotmail.com>
2021-02-16 20:53:59 -0400

    no permitir uso del bot de parte de otros usuarios

--- index.js
+++ index.js
@@ -8,39 +8,38 @@ client.on('ready', () => {
 
 //const prefix = process.env.PREFIX;
 let prefix = "jkr!";
+const username = "StunxFS";
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || !message.guild) return;
     if (message.author.bot) return;
+    if (message.author.username != username) {
+        message.channel.send('No eres mi dueño (StunxFS), asi que no me toques UnU');
+        return;
+    }
     
     const cont = message.content.split(' ').slice(1);
     const args = cont.join(' ');
     
-    if (message.content.startsWith(prefix+'ban_to_night')) {
+    if (message.content.startsWith(prefix + 'ban_to_night')) {
     	message.member.roles.add("Vicepresidente Ejecutivo");
   		message.channel.send("banning...");
     }
     
-    /*if (message.content.startsWith(prefix+'ping')) {
+    if (message.content.startsWith(prefix + 'ping')) {
         message.channel.send('pong');
     }
         
-    if (message.content.startsWith(prefix+'que_es_draky')) {
-            message.channel.send('**No conozco a ese pendejo :v**');
-    }*/
-        
-   /* if (message.content.startsWith(prefix+ 'say')) {
-            if (!args) return;
-            message.channel.bulkDelete(1);
-            message.channel.send(args);
-    }*/
+    if (message.content.startsWith(prefix + 'say')) {
+        if (!args) return;
+        message.channel.bulkDelete(1);
+        message.channel.send(args);
+    }
     
-    if (message.author.username == "StunxFS" && message.content.startsWith(prefix + "purge")) {
+    if (message.content.startsWith(prefix + "purge")) {
     	if (!args) return;
 		let cantidad = parseInt(args[0]);
 		message.channel.bulkDelete(cantidad);
-	} else {
-		message.channel.send('No eres StunxFS :/');
 	}
 });
 
